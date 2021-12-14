983d79c90bc9b28d5b02eb68dced5945a2e7cb3a
Blackjackdeath <89104602+Blackjackdeath@users.noreply.github.com>
2021-12-14 19:35:58 +0200

    add homework

---
+++ homework/script.js
@@ -0,0 +1,40 @@
+const movie1Title = "Venom: Let There Be Carnage";
+let movie1Rating = 7.2;
+const movie1PosterUrl ="https://image.tmdb.org/t/p/w1280//rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg";
+
+const movie2Title = "Clifford the Big Red Dog";
+let movie2Rating = 7.5;
+const movie2PosterUrl ="https://image.tmdb.org/t/p/w1280//ygPTrycbMSFDc5zUpy4K5ZZtQSC.jpg";
+
+const movie1={
+    title:"Venom: Let There Be Carnage",
+    raiting:7.2,
+    url:"https://image.tmdb.org/t/p/w1280//rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg"
+};
+
+const movie2={
+    title:"Clifford the Big Red Dog",
+    raiting:7.5,
+    url:"https://image.tmdb.org/t/p/w1280//ygPTrycbMSFDc5zUpy4K5ZZtQSC.jpg"
+};
+
+console.log(movie1.title);
+console.log(movie2.title);
+
+const arr=['Nissan','Jeep','Tesla','ZIL-131','HAZ-66'];
+console.log(arr.length);
+console.log(arr[arr.length-1]);
+
+const hapak={
+    name: "Ivan",
+    age:32,
+    soldier:true
+};
+console.log(hapak);
+
+const muckachevo={
+    history:"Понад 1100 років минуло з часу першої писемної згадки про місто. У літописі Аноніма (Аноніма нотаріуса, Аноніма хроніста Бели ІІІ) «Діяння угорців» зазначено про перехід 896 року угорського вождя Алмоша (Арпада) через Карпати й захоплення Мукачева, де він 40 днів святкував перемогу. 1263 року в грамоті королевича Іштвана V, сина короля Бели IV, ідеться про те, що село Чинадійово розташовано вище від міста Мукачева.",
+    age:1025,
+    castle:true
+};
+console.log(muckachevo);
